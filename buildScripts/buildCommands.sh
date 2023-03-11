9a176d48b81eeaed897288392deb7c6141cd0385
Jake Chavis <jakechavis6@gmail.com>
2023-03-10 20:47:02 -0500

    Updating build cocmmands

--- buildScripts/buildCommands.sh
+++ buildScripts/buildCommands.sh
@@ -7,4 +7,4 @@ mvn -s settings.xml clean package
 echo "Package build successfully. Deploying version now"
 aws codeartifact delete-package-versions --domain slickpickbets-backend --repository SlickPickBets --format maven --domain-owner 591565668473 --namespace com.slickpickbets --package shared-models --versions 1.0.1
 echo "Just deleted existing package, now we are set to re-deploy package"
-mvn -s settings.xml clean package deploy
+mvn -s settings.xml clean package Deploying
