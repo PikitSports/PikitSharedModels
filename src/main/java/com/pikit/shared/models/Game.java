cfabacc2dcfeb4cba73a7f4e4e6a548cba47c957
Jake Chavis <jakechavis6@gmail.com>
2023-04-11 19:02:06 -0400

    Adding deserialization correctly for game object

--- src/main/java/com/pikit/shared/models/Game.java
+++ src/main/java/com/pikit/shared/models/Game.java
@@ -1,5 +1,6 @@
 package com.pikit.shared.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import java.util.HashMap;
@@ -25,11 +26,13 @@ public class Game {
     private static final String FINAL = "Final";
     private static final String OVER_UNDER = "overUnder";
 
+    //Fields to serialize
     private HashMap<String, String> gameStats;
     private HashMap<String, String> homeTeamStats;
     private HashMap<String, String> awayTeamStats;
     private HashMap<String, String> bettingStats;
 
+    @JsonIgnore
     public String getGameId() {
         String homeTeam = gameStats.get(HOME_TEAM);
         String awayTeam = gameStats.get(AWAY_TEAM);
@@ -39,46 +42,57 @@ public class Game {
         return String.format("%s|%s|%s|%s", homeTeam, awayTeam, gameDate, numGameForDay);
     }
 
+    @JsonIgnore
     public String getGameWinner() {
         return gameStats.get(GAME_WINNER);
     }
 
+    @JsonIgnore
     public String getHomeTeam() {
         return gameStats.get(HOME_TEAM);
     }
 
+    @JsonIgnore
     public String getAwayTeam() {
         return gameStats.get(AWAY_TEAM);
     }
 
+    @JsonIgnore
     public String getTeamThatCoveredTheSpread() {
         return gameStats.get(TEAM_THAT_COVERED_THE_SPREAD);
     }
 
+    @JsonIgnore
     public Double getHomeTeamMoneyLine() {
         return getSanitizedDouble(bettingStats.get(HOME_TEAM_MONEY_LINE));
     }
 
+    @JsonIgnore
     public Double getAwayTeamMoneyLine() {
         return getSanitizedDouble(bettingStats.get(AWAY_TEAM_MONEY_LINE));
     }
 
+    @JsonIgnore
     public Double getHomeTeamSpread() {
         return getSanitizedDouble(bettingStats.get(HOME_TEAM_SPREAD));
     }
 
+    @JsonIgnore
     public Double getAwayTeamSpread() {
         return getSanitizedDouble(bettingStats.get(AWAY_TEAM_SPREAD));
     }
 
+    @JsonIgnore
     public Double getOverUnder() {
         return getSanitizedDouble(bettingStats.get(OVER_UNDER));
     }
 
+    @JsonIgnore
     public Double getHomeTeamScore() {
         return getSanitizedDouble(homeTeamStats.get(FINAL));
     }
 
+    @JsonIgnore
     public Double getAwayTeamScore() {
         return getSanitizedDouble(awayTeamStats.get(FINAL));
     }
