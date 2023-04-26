012c9053b9c1e41e5802b9ce26772a62daedc939
Jake Chavis <jakechavis6@gmail.com>
2023-04-26 12:55:56 +0200

    Updating data source config

--- src/main/java/com/pikit/shared/dao/s3/model/DataSourceConfig.java
+++ src/main/java/com/pikit/shared/dao/s3/model/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.pikit.shared.dao.s3.model;
 
+import com.pikit.shared.enums.League;
 import com.pikit.shared.models.StatMetadata;
 import lombok.*;
 
@@ -12,6 +13,7 @@ import java.util.HashMap;
 @NoArgsConstructor
 @ToString
 public class DataSourceConfig {
+    private League league;
     private String startSeason;
     private String endSeason;
     private HashMap<String, StatMetadata> statsAvailable;
--- src/main/java/com/pikit/shared/datasource/DataSourceConstants.java
+++ src/main/java/com/pikit/shared/datasource/DataSourceConstants.java
@@ -5,9 +5,14 @@ import com.pikit.shared.enums.League;
 public final class DataSourceConstants {
     private DataSourceConstants() {}
 
-    public static final String NFL_CURRENT_SEASON = "2023";
-    public static final String MLB_CURRENT_SEASON = "2023";
-    public static final String NBA_CURRENT_SEASON = "2022";
+    private static final String DATASOURCE_CONFIG_PATH = "%s/dataSourceConfig.json";
+    private static final String STAT_CONFIGURATION_PATH = "%s/statConfiguration.json";
+    private static final String STANDINGS_PATH = "%s/standings/%s/%s.json";
+    private static final String TEAM_METADATA_PATH = "%s/teamMetadata/%s.json";
+
+    private static final String NFL_CURRENT_SEASON = "2023";
+    private static final String MLB_CURRENT_SEASON = "2023";
+    private static final String NBA_CURRENT_SEASON = "2022";
 
     public static String getCurrentSeasonForLeague(League league) {
         switch (league) {
@@ -16,5 +21,21 @@ public final class DataSourceConstants {
             case NBA: return NBA_CURRENT_SEASON;
             default: throw new RuntimeException("Invalid league: " + league);
         }
+    }
+
+    public static String getDataSourcePathForLeague(League league) {
+        return String.format(DATASOURCE_CONFIG_PATH, league);
+    }
+
+    public static String getStatConfigurationPathForLeague(League league) {
+        return String.format(STAT_CONFIGURATION_PATH, league);
+    }
+
+    public static String getStandingsPathForLeague(League league, String statName, String season) {
+        return String.format(STANDINGS_PATH, league, statName, season);
+    }
+
+    public static String getTeamMetadataPathForLeague(League league, String season) {
+        return String.format(TEAM_METADATA_PATH, league, season);
     }
 }
